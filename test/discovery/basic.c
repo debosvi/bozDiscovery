a32e7607f1d165c06a88dbdbf9708123dc5ee349
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2013-10-29 23:04:41 +0100

    test basically discovery lib

---
+++ test/discovery/basic.c
@@ -0,0 +1,30 @@
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "discovery.h"
+
+int main(int ac, char **av) {
+    int ret=0;
+    boz_disc_t manager;
+    
+    (void)ac;
+    (void)av;
+    
+    ret=boz_discovery_init(&manager);
+    if(ret<0) {
+        fprintf(stderr, "Unable to init discovery manager, errno(%d/%s)\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    ret=boz_discovery_term(&manager);
+    if(ret<0) {
+        fprintf(stderr, "Unable to term discovery manager, errno(%d/%s)\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+        
+    fprintf(stdout, "All test succeeded\n");
+    exit(EXIT_SUCCESS);
+}
